7000c52303cdebba60c148ee8a7874574b9bd399
Cheulong Sear <searcheulong@gmail.com>
2024-08-19 18:22:55 +0700

    create mongoose schema for contact

---
+++ src/api/v1/models/contactModel.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Contact", contactSchema);
