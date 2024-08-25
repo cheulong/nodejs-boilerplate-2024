20e914b5b0eec740461a45d435f6a3543f9ff02a
Cheulong Sear <searcheulong@gmail.com>
2024-08-25 23:03:54 +0700

    add auth swagger

--- src/api/v1/routes/contactRoutes.ts
+++ src/api/v1/routes/contactRoutes.ts
@@ -66,7 +66,98 @@ const router = Router();
  *      404:
  *        description: Not found
  */
+
 router.route("/").get(getAllContacts).post(createNewContact);
+
+/**
+ * @openapi
+ * /api/contacts/{id}:
+ *  get:
+ *    summary: Get a contact
+ *    tags:
+ *      - Contact
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *    responses:
+ *      200:
+ *        description: Successfully fetched all contacts
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Internal server error
+ *      400:
+ *        description: Bad request
+ *      403:
+ *        description: Forbidden
+ *      404:
+ *        description: Not found
+ *  put:
+ *    summary: update a contact
+ *    tags:
+ *      - Contact
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                example: "doggie"
+ *              email:
+ *                type: string
+ *                example: "name"
+ *              phone:
+ *                type: string
+ *                example: "1234"
+ *    responses:
+ *      200:
+ *        description: Successfully fetched all contacts
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Internal server error
+ *      400:
+ *        description: Bad request
+ *      403:
+ *        description: Forbidden
+ *      404:
+ *        description: Not found
+ *  delete:
+ *    summary: delete a contact
+ *    tags:
+ *      - Contact
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *    responses:
+ *      200:
+ *        description: Successfully fetched all contacts
+ *      401:
+ *        description: Unauthorized
+ *      500:
+ *        description: Internal server error
+ *      400:
+ *        description: Bad request
+ *      403:
+ *        description: Forbidden
+ *      404:
+ *        description: Not found
+ */
 
 router
   .route("/:id")
